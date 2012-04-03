c8e7c66eb71477e968aab02ab63001c2c2adcd78
Stefan Zhelyazkov <stz@seas.upenn.edu>
2012-04-02 23:08:18 -0400

    added login functionality

---
+++ login_user.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'connect.php';
+
+$user = trim($_POST['email']);
+$pass = md5(trim($_POST['password']));
+
+$query = "SELECT * FROM `users` WHERE `user` = '$user' AND `password` = '$pass'";
+$query = mysql_query($query);
+while($result = mysql_fetch_array($query)){
+   echo $result['first_name'].' '.$result['last_name']." logged in!";
+}
+
+if(!$query){
+   echo "The user does not exist.";
+}
+
+?>
