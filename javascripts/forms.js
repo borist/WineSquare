a2b096f52d7623ffe8230ca037d647b9767fa16c
Boris Treskunov <borist@seas.upenn.edu>
2012-04-03 00:28:56 -0400

    added some more error handling for signup page

--- javascripts/forms.js
+++ javascripts/forms.js
@@ -3,21 +3,31 @@ function validateAccount(){
 		  $email = emailPattern.test($("#email").val());
 		  $password = $("#password1").val() == $("#password2").val();
 		  $password = $password && $("#password1").val() != "";
-	
-		  if (!$email && !$password)
+		  $fname = $("#firstname").val() == "";
+		  $lname = $("#lastname").val() == "";
+		
+		  if ($fname && $lname)
 				return 1;
-			else if (!$email)
+		  else if ($fname)
 				return 2;
-			else if (!$password)
+		  else if ($lname)
 				return 3;
+		  if (!$email && !$password)
+				return 4;
+			else if (!$email)
+				return 5;
+			else if (!$password)
+				return 6;
 			else return 0;
 		} 
 	
 		function clearAlerts(){
 				$("#alert").css("display", "none");
-				$("#email").removeClass("red");
-				$("#password1").removeClass("red");			
-				$("#password2").removeClass("red");
+				$("#fname_wrapper").removeClass("error");
+				$("#lname_wrapper").removeClass("error");				
+				$("#email").removeClass("error");
+				$("#password1").removeClass("error");			
+				$("#password2").removeClass("error");
 		}
 	
 		function showAlert(input) {	
@@ -25,22 +35,35 @@ function validateAccount(){
 				case 0:
 					return;
 				break;
-				case 1: 
+				case 1:
+					$("#alert").text("Please fill in the blank fields");
+					$("#fname_wrapper").addClass("error");
+					$("#lname_wrapper").addClass("error");					
+				break;
+				case 2:
+					$("#alert").text("Please fill in the blank fields");
+					$("#fname_wrapper").addClass("error");					
+				break;
+				case 3:
+					$("#alert").text("Please fill in the blank fields");
+					$("#lname_wrapper").addClass("error");					
+				break;								
+				case 4: 
 					$("#alert").text("You haven't provided a valid email address and your passwords do not match.");
 					$("#email_wrapper").addClass("error");
 					$("#pword1_wrapper").addClass("error");
 					$("#pword2_wrapper").addClass("error");
 					break;
-				case 2: 
+				case 5: 
 					$("#alert").text("Your haven't provided a valid email address.");
 					$("#email_wrapper").addClass("error");
 					break;
-				case 3:
+				case 6:
 					$("#alert").text("Your passwords don't match.");
 					$("#pword1_wrapper").addClass("error");
 					$("#pword2_wrapper").addClass("error");
 					break;
-				case 4:
+				case 7:
 					$("#alert").text("There was an error with the database. Please try again.");
 					break;
 				default: break;	
